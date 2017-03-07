d576e75de40c9b2215039d2a4d989c8faec374d8
Ray Hansen <RayHansenJr@gmail.com>
2017-03-06 19:10:25 -0600

    advanced-first-web-server updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // Your server code here...
+import express from 'express';
+
+// creating my new instance of express
+const app = express();
+
+app.get('/contacts', (request, response) => {
+  return response.json(request['contact']);
+});
+
+// declaring the route
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+// setting port
+const PORT = 3001;
+
+// Telling instance of express to listen to request made on our port
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Error', err);
+  }
+  return console.log('Listening on: http://localhost:' + PORT);
+});
